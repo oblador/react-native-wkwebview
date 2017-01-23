d856f451fb1cf9ab8a5af1e816aea35a0c373fbf
Joel Arvidsson <joel@oblador.se>
2017-01-23 20:59:25 +0100

    Enable inline media playback

--- ios/RCTWKWebView/RCTWKWebView.m
+++ ios/RCTWKWebView/RCTWKWebView.m
@@ -40,7 +40,9 @@
     WKUserContentController* userController = [[WKUserContentController alloc]init];
     [userController addScriptMessageHandler:self name:@"reactNative"];
     config.userContentController = userController;
-    
+    config.allowsInlineMediaPlayback = YES;
+    config.requiresUserActionForMediaPlayback = NO;
+
     _webView = [[WKWebView alloc] initWithFrame:self.bounds configuration:config];
     _webView.UIDelegate = self;
     _webView.navigationDelegate = self;
